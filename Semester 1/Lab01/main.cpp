4bae0ac433fca40118ef42c1e1b1aea7b9409298
Eugenius <mylnikov_evgeniy90@mail.ru>
2026-09-19 14:21:36 +0200

    UPD: Lab01 main.cpp

--- Semester 1/Lab01/main.cpp
+++ Semester 1/Lab01/main.cpp
@@ -2,39 +2,57 @@
 #include <climits>
 #include <cfloat>
 
+/*
+ТЕОРИЯ:
+Переменная - некоторая область памяти, которая обладает именем, 
+она предназначена для хранения данных определённого типа,
+по имени можно обращаться к переменной.
+
+Типы данных в C++:
+- целые знаковые (char, short, int, long long)
+- целые беззнаковые (unsigned char, unsigned short, unsigned int, unsigned long long)
+- вещественные (float, double)
+- символьный (char)
+- логический (bool)
+
+Знаковый тип (signed) может хранить как положительные, так и отрицательные числа.
+Беззнаковый тип (unsigned) хранит только неотрицательные числа (ноль и положительные).
+За счёт этого максимальное значение у беззнакового типа больше.
+*/
+
 int main() {
-    char symbol = 'M';
-    short number = -5555;
-    int bigNumber = -123456789;
+    char symbol = 'E';
+    short number = -5000;
+    int bigNumber = -11223344;
     long long veryBigNumber = 999999999;
-    unsigned int unsignedNumber = 987654321;
+    unsigned int unsignedNumber = 123456789;
     float numberPi = 3.141592f;
     double doubleNumber = 3.1082008;
-    bool brain = true;
+    bool brain = true + 100;
     
     std::cout << "char " << symbol << " " << sizeof(symbol) << " " 
-              << CHAR_MIN << " " << CHAR_MAX << "\n";
+              << CHAR_MIN << " " << CHAR_MAX << std::endl;
               
     std::cout << "short " << number << " " << sizeof(number) << " " 
-              << SHRT_MIN << " " << SHRT_MAX << "\n";
+              << SHRT_MIN << " " << SHRT_MAX << std::endl;
               
     std::cout << "int " << bigNumber << " " << sizeof(bigNumber) << " " 
-              << INT_MIN << " " << INT_MAX << "\n";
+              << INT_MIN << " " << INT_MAX << std::endl;
               
     std::cout << "long_long " << veryBigNumber << " " << sizeof(veryBigNumber) << " " 
-              << LLONG_MIN << " " << LLONG_MAX << "\n";
+              << LLONG_MIN << " " << LLONG_MAX << std::endl;
               
     std::cout << "unsigned_int " << unsignedNumber << " " << sizeof(unsignedNumber) << " " 
-              << 0 << " " << UINT_MAX << "\n";
+              << 0 << " " << UINT_MAX << std::endl;
               
     std::cout << "float " << numberPi << " " << sizeof(numberPi) << " " 
-              << -FLT_MAX << " " << FLT_MAX << "\n";
+              << -FLT_MAX << " " << FLT_MAX << std::endl;
               
     std::cout << "double " << doubleNumber << " " << sizeof(doubleNumber) << " " 
-              << -DBL_MAX << " " << DBL_MAX << "\n";
+              << -DBL_MAX << " " << DBL_MAX << std::endl;
               
     std::cout << "bool " << brain << " " << sizeof(brain) << " " 
-              << 0 << " " << 1 << "\n";
+              << 0 << " " << 1 << std::endl;
 
     return 0;
 }
